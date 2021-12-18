af0821ffa7e8cad99ab6d6e07333c7158269051c
Maksim Zuev <maksim.zuev@jetbrains.com>
2021-12-18 23:48:53 +0300

    Fix private variables

--- A02.cpp
+++ A02.cpp
@@ -20,10 +20,11 @@ std::vector<std::vector<int>> multiply(const std::vector<std::vector<int>>& a, c
     for (int i = 0; i < size; ++i) {
         c[i].resize(size, 0);
     }
-#pragma omp parallel num_threads(threads)
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            for (int k = 0; k < size; ++k) {
+    int i, j, k;
+#pragma omp parallel for num_threads(threads) private(i,j,k)
+    for (i = 0; i < size; ++i) {
+        for (j = 0; j < size; ++j) {
+            for (k = 0; k < size; ++k) {
                 c[i][j] += a[i][k] * b[k][j];
             }
         }
