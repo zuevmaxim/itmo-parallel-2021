8a26e382d1766dd5b5837692e9b6ce4cca7efc91
Maksim Zuev <maksim.zuev@jetbrains.com>
2021-12-24 23:27:34 +0300

    Add task 17

---
+++ Assignment17.cpp
@@ -0,0 +1,37 @@
+#include "mpi.h"
+#include <iostream>
+int main(int argc, char **argv)
+{
+    int size, rank, position, i;
+    float a[10];
+    char b[10], buf[100];
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::string s = std::to_string(rank) + " ";
+    for (i = 0; i < 10; i++) {
+        a[i] = rank + 1.0;
+        if (rank == 0) b[i] = 'a';
+        else b[i] = 'b';
+        s += std::to_string(a[i]) + " " + b[i] + " ";
+    }
+    std::cout << s << std::endl;
+    position = 0;
+    if (rank == 0) {
+        MPI_Pack(a, 10, MPI_FLOAT, buf, 100, &position, MPI_COMM_WORLD);
+        MPI_Pack(b, 10, MPI_CHAR, buf, 100, &position, MPI_COMM_WORLD);
+        MPI_Bcast(buf, 100, MPI_PACKED, 0, MPI_COMM_WORLD);
+    }
+    else {
+        MPI_Bcast(buf, 100, MPI_PACKED, 0, MPI_COMM_WORLD);
+        position = 0;
+        MPI_Unpack(buf, 100, &position, a, 10, MPI_FLOAT, MPI_COMM_WORLD);
+        MPI_Unpack(buf, 100, &position, b, 10, MPI_CHAR, MPI_COMM_WORLD);
+    }
+    s = std::to_string(rank) + " ";
+    for (i = 0; i < 10; i++) {
+        s += std::to_string(a[i]) + " " + b[i] + " ";
+    }
+    std::cout << s << std::endl;
+    MPI_Finalize();
+}
